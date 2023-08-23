1664a7a52ff7d723eda8cee64da854ae883c57ea
Spencer Merrill <82860126+Canadianfaller7@users.noreply.github.com>
2023-08-23 12:05:47 -0600

    set up mutations for login on client and server. (#3)
    Converted login form to use graphql mutations instead of REST API (#3)

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,25 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password){
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          link
+        }
+      }
+    }
+  }
+  `;
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
