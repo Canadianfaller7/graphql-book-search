345d667061a4a92f204603d55c451ed512605e31
Spencer Merrill <canadianfaller7@gmail.com>
2023-08-23 10:12:15 -0600

    connected graphql and apollo to frontend and updated dependencies

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
+  // process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
