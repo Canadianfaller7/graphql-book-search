05762a9bc00b4c832fe8f1c188c0f7fd328ea97b
Spencer Merrill <82860126+Canadianfaller7@users.noreply.github.com>
2023-08-23 10:22:20 -0600

    connected graphql and apollo to frontend and updated dependencies (#1)
    * connected graphql and apollo to frontend and updated dependencies
    
    * fixed bootstrap version and localhost url in config connection.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from "@apollo/client";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from "@apollo/client/link/context";
 
 function App() {
+  const httpLink = createHttpLink({
+    uri: "/graphql",
+  });
+
+  const authLink = setContext((_, {headers}) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem("id_token");
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +49,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks", {
+  // process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
